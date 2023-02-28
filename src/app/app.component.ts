9fa11b1ac343d19d9bc4ec1dd500733de01096e1
Santhosh <shanmugamsanthosh22@gmail.com>
2023-03-01 02:18:57 +0530

    cjbsjbdj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements OnInit {
   title = 'musicplayer-application';
 
   ngOnInit(){
-    
+    window.onbeforeunload;
     $( function() {
       var availableTags = [
         
